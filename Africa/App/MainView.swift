241349dea856816bf9a68fd9a139116aeccd85a4
shouta0715 <kshouta0715@gmail.com>
2025-01-11 14:12:37 +0900

    add: tab view

--- Africa/App/MainView.swift
+++ Africa/App/MainView.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      TabView {
+        Tab("Browse",systemImage: "square.grid.2x2") {
+          ContentView()
+        }
+        
+        Tab("Watch",systemImage: "play.rectangle") {
+          VideoListView()
+        }
+        
+        Tab("Map",systemImage: "map") {
+          MapView()
+        }
+        
+        Tab("Gallery",systemImage: "photo") {
+          GalleryView()
+        }
+      } //: TabView
     }
 }
 
